9a334e7890a56d05a46bebb87bf35fcf114a0b2a
Chia Wen Xuen <wenxuen6688@gmail.com>
2022-10-25 14:06:49 +0800

    modified:   .vscode/launch.json 	modified:   bin/main/java/Main.class 	modified:   bin/main/java/controllers/CommonMethods.class 	modified:   bin/main/java/controllers/LoginController.class 	modified:   bin/main/java/entities/Car.class 	modified:   bin/main/java/entities/Log.class 	modified:   bin/main/java/entities/Order.class 	modified:   bin/main/java/entities/User.class 	modified:   bin/main/java/util/EncryptionService.class 	modified:   bin/main/java/util/FileService.class 	modified:   bin/main/java/util/ValidationService.class 	modified:   src/main/java/entities/User.java

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,3 +1,41 @@
 package main.java.entities;
 
-public class User {}
+public class User {
+    private String username;
+    private String userId;
+    private String passsword;
+    private boolean loginStatus;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPasssword() {
+        return passsword;
+    }
+
+    public void setPasssword(String passsword) {
+        this.passsword = passsword;
+    }
+
+    public boolean isLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(boolean loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+}
