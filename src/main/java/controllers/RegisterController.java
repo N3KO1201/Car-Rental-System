4a00fd9989918807e04d5a53730f763ef4cd2b97
Chia Wen Xuen <wenxuen6688@gmail.com>
2022-11-03 17:12:56 +0800

    Register Page FXML created

--- src/main/java/controllers/RegisterController.java
+++ src/main/java/controllers/RegisterController.java
@@ -1,17 +1,34 @@
 package main.java.controllers;
 
+import java.io.IOException;
+import java.lang.StackWalker.Option;
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import main.java.entities.User;
 
 public class RegisterController {
 
     @FXML
-    private Button cBtn;
+    private Button regBtn;
 
     @FXML
-    private Button rBtn;
+    private Button clearBtn;
+
+    @FXML
+    private Button retBtn;
 
     @FXML
     private TextField txtContact;
@@ -23,11 +40,43 @@ public class RegisterController {
     private TextField txtName;
 
     @FXML
-    private TextField txtPassword;
+    private PasswordField txtPassword;
 
     @FXML
-    void handleActio(ActionEvent event) {
+    private PasswordField txtPassC;
 
+    private void clear() {
+        txtName.clear();
+        txtMail.clear();
+        txtContact.clear();
+        txtPassword.clear();
+        txtPassC.clear();
+    }
+
+    private int idGenerator() {
+        return (int) (Math.random() * 100);
+    }
+
+    @FXML
+    public void clear(ActionEvent event) {
+        clear();
+    }
+
+    @FXML
+    public void register(ActionEvent event) throws IOException {
+        User tempUser = new User(idGenerator(), txtName.getText(), txtPassword.getText(), txtMail.getText(),
+                txtContact.getText(), false);
+        clear();
+        JOptionPane.showConfirmDialog(null, "Returning to Login Page");
+
+        // add data to userfile
+
+        // return to Login page
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
