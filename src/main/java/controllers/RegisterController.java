3cf2bcf482be165a589947ce8e33a869fd0fbcab
Chia Wen Xuen <wenxuen6688@gmail.com>
2022-11-03 14:23:42 +0800

    stash

---
+++ src/main/java/controllers/RegisterController.java
@@ -0,0 +1,33 @@
+package main.java.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+
+    @FXML
+    private Button cBtn;
+
+    @FXML
+    private Button rBtn;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtMail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    void handleActio(ActionEvent event) {
+
+    }
+
+}
