5595c661dc0ed2ed591f14913a3bf2e17239f8ac
Chia Wen Xuen <wenxuen6688@gmail.com>
2022-12-12 15:05:45 +0800

    guest page added

---
+++ src/main/java/controllers/GuestController.java
@@ -0,0 +1,63 @@
+package main.java.controllers;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import main.java.dao.UserDao;
+import main.java.entities.Car;
+
+public class GuestController extends CommonMethods implements UserDao {
+
+    @FXML
+    private TableView<Car> availableCarTable;
+
+    @FXML
+    private Button exitBtn;
+
+    @FXML
+    private Hyperlink registerLink;
+
+    @FXML
+    private TableColumn<Car, String> rentBrandCol;
+
+    @FXML
+    private TableColumn<Car, Integer> rentCarId;
+
+    @FXML
+    private TableColumn<Car, String> rentCarStatus;
+
+    @FXML
+    private TableColumn<Car, Double> rentCostCol;
+
+    @FXML
+    private TableColumn<Car, String> rentModelCol;
+
+    @FXML
+    private TableColumn<Car, String> rentPlateNumCol;
+
+    @FXML
+    private TableColumn<Car, Integer> rentYearCol;
+
+    @FXML
+    void registerPage(ActionEvent event) {
+        super.loadLinkScene(event);
+    }
+
+    @Override
+    public void login(ActionEvent e) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void register(ActionEvent e) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
